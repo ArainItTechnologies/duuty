4db80fa46527d3173b9c470eac55158f55b90c16
Prasanna Dommalapati <prasanna.dommalapati@gmail.com>
2024-12-12 22:23:53 +0000

    auto image slider

--- DuutyApp/duutyapp.client/src/Components/Carousel.jsx
+++ DuutyApp/duutyapp.client/src/Components/Carousel.jsx
@@ -1,10 +1,17 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import { motion, AnimatePresence } from "framer-motion";
 
 const Carousel = ({ images }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
-  const [direction, setDirection] = useState(null);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, []);
 
   const slideVariants = {
     hiddenRight: {
@@ -30,111 +37,28 @@ const Carousel = ({ images }) => {
       },
     },
   };
-  const slidersVariants = {
-    hover: {
-      scale: 1.2,
-      backgroundColor: "#ff00008e",
-    },
-  };
-  const dotsVariants = {
-    initial: {
-      y: 0,
-    },
-    animate: {
-      y: -10,
-      scale: 1.2,
-      transition: { type: "spring", stiffness: 1000, damping: "10" },
-    },
-    hover: {
-      scale: 1.1,
-      transition: { duration: 0.2 },
-    },
-  };
-
-  const handleNext = () => {
-    setDirection("right");
-    setCurrentIndex((prevIndex) =>
-      prevIndex + 1 === images.length ? 0 : prevIndex + 1
-    );
-  };
-
-  const handlePrevious = () => {
-    setDirection("left");
-
-    setCurrentIndex((prevIndex) =>
-      prevIndex - 1 < 0 ? images.length - 1 : prevIndex - 1
-    );
-  };
-
-  const handleDotClick = (index) => {
-    setDirection(index > currentIndex ? "right" : "left");
-    setCurrentIndex(index);
-  };
 
   return (
     <div className="carousel">
-        <div className="carousel-images">
+      <div className="carousel-images">
         <AnimatePresence>
           <motion.img
-            key={currentIndex}
             src={images[currentIndex]}
-            initial={direction === "right" ? "hiddenRight" : "hiddenLeft"}
-            animate="visible"
-            exit="exit"
+            key={images[currentIndex]}
+            alt={`Slide ${currentIndex + 1}`}
+            initial={{ opacity: 0, x: 100 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, x: -100 }}
+            transition={{ duration: 0.5 }}
             variants={slideVariants}
           />
         </AnimatePresence>
-        <div className="slide_direction">
-          <motion.div
-            variants={slidersVariants}
-            whileHover="hover"
-            className="left"
-            onClick={handlePrevious}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              height="20"
-              viewBox="0 96 960 960"
-              width="20"
-            >
-              <path d="M400 976 0 576l400-400 56 57-343 343 343 343-56 57Z" />
-            </svg>
-          </motion.div>
-          <motion.div
-            variants={slidersVariants}
-            whileHover="hover"
-            className="right"
-            onClick={handleNext}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              height="20"
-              viewBox="0 96 960 960"
-              width="20"
-            >
-              <path d="m304 974-56-57 343-343-343-343 56-57 400 400-400 400Z" />
-            </svg>
-          </motion.div>
-        </div>
-      </div>
-      <div className="carousel-indicator">
-        {images.map((_, index) => (
-          <motion.div
-            key={index}
-            className={`dot ${currentIndex === index ? "active" : ""}`}
-            onClick={() => handleDotClick(index)}
-            initial="initial"
-            animate={currentIndex === index ? "animate" : ""}
-            whileHover="hover"
-            variants={dotsVariants}
-          ></motion.div>
-        ))}
       </div>
     </div>
   );
 };
 
 Carousel.propTypes = {
-  images: PropTypes.array.isRequired
-}
+  images: PropTypes.array.isRequired,
+};
 export default Carousel;
