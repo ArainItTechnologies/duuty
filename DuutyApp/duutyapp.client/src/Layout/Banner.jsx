6a8693587bec861b63ba3ecf750b1519202ca421
Prasanna Dommalapati <prasanna.dommalapati@gmail.com>
2025-01-23 02:17:55 +0000

    banner hide option

--- DuutyApp/duutyapp.client/src/Layout/Banner.jsx
+++ DuutyApp/duutyapp.client/src/Layout/Banner.jsx
@@ -1,13 +1,35 @@
+import { useState } from "react";
 import { useTranslation } from "../translations/TranslationHook";
 import { GoRocket } from "react-icons/go";
+import { FaRegTimesCircle } from "react-icons/fa";
 
 const Banner = () => {
   const { t } = useTranslation();
+  const [isVisible, setIsVisible] = useState(true);
+
+  const hideBanner = () => setIsVisible(false);
+
   return (
-    <div className="banner-section">
-      <h3 className="banner-heading">{t("banner")} <GoRocket size={30} style={{ paddingLeft:"5px", color: 'yellow'}}/></h3> 
-      {/* <p className="banner-slogan">{t("slogan")}</p> */}
-    </div>
+    isVisible && (
+      <div className="banner-section">
+        <h3 className="banner-heading">
+          {t("banner")}
+          <GoRocket size={30} style={{ paddingLeft: "5px", color: "yellow" }} />
+          <FaRegTimesCircle
+            size={15}
+            style={{
+              position: "absolute",
+              top: "10px",
+              right: "10px",
+              cursor: "pointer",
+              color: "white",
+            }}
+            onClick={hideBanner} // Hide the banner when clicked
+          />
+        </h3>
+        {/* <p className="banner-slogan">{t("slogan")}</p> */}
+      </div>
+    )
   );
 };
 
