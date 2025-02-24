102df52f10b529a290cb2bc9e0d33134f616af34
ArainItTechnologies <155706274+ArainItTechnologies@users.noreply.github.com>
2025-02-24 03:00:15 +0000

    Terraform deploy (#13)
    * tf-deploy-debug
    
    * output
    
    * providers
    
    * remove backend
    
    * no vars
    
    * add terraform
    
    ---------
    
    Co-authored-by: Prasanna Dommalapati <prasanna.dommalapati@gmail.com>

--- .infrastructure/main.tf
+++ .infrastructure/main.tf
@@ -10,18 +10,17 @@ resource "azurerm_service_plan" "app_plan" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   os_type             = "Windows"
-  sku_name            = "S1"
+  sku_name            = "B1"
 }
 
 # Create App Service
-resource "azurerm_windows_web_app" "app_service" {
+resource "azurerm_linux_web_app" "app_service" {
   name                = var.app_service_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   service_plan_id     = azurerm_service_plan.app_plan.id
 
   site_config {
-    dotnet_version = "v8.0"
   }
 
   app_settings = {
@@ -36,4 +35,22 @@ resource "azurerm_storage_account" "storage_account" {
   location                 = azurerm_resource_group.rg.location
   account_tier              = "Standard"
   account_replication_type = var.storage_account_sku
+}
+
+# Create SQL Server
+resource "azurerm_mssql_server" "sql_server" {
+  name                         = var.sql_server_name
+  resource_group_name           = azurerm_resource_group.rg.name
+  location                     = azurerm_resource_group.rg.location
+  version                      = "12.0"
+  administrator_login          = var.sql_admin_username
+  administrator_login_password = var.sql_admin_password
+}
+
+# Create SQL Database
+resource "azurerm_mssql_database" "sql_database" {
+  name                = var.sql_database_name
+  server_id           = azurerm_mssql_server.sql_server.id
+  sku_name            = "S1"
+  collation           = "SQL_Latin1_General_CP1_CI_AS"
 }
--- .infrastructure/variables.tf
+++ .infrastructure/variables.tf
@@ -3,7 +3,7 @@ variable "resource_group_name" {
 }
 
 variable "location" {
-  default = "East US"
+  default = "West US"
 }
 
 variable "app_service_plan_name" {
@@ -19,5 +19,31 @@ variable "storage_account_name" {
 }
 
 variable "storage_account_sku" {
-  default = "Standard_LRS"
+  default = "LRS"
 }
+
+variable "sql_server_name" {
+  description = "The name of the SQL Server"
+  type        = string
+}
+
+variable "sql_database_name" {
+  description = "The name of the SQL Database"
+  type        = string
+}
+
+variable "sql_admin_username" {
+  description = "The admin username for the SQL Server"
+  type        = string
+}
+
+variable "sql_admin_password" {
+  description = "The admin password for the SQL Server"
+  type        = string
+  sensitive   = true
+}
+
+variable "subscription_id" {}
+variable "tenant_id" {}
+variable "client_id" {}
+variable "client_secret" {}
