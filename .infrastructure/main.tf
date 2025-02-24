8f606a4c184cf10f12290b14c88997f80fef1e7b
Prasanna Dommalapati <prasanna.dommalapati@gmail.com>
2025-02-24 02:50:38 +0000

    add terraform

--- .infrastructure/main.tf
+++ .infrastructure/main.tf
@@ -10,7 +10,7 @@ resource "azurerm_service_plan" "app_plan" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   os_type             = "Windows"
-  sku_name            = "S1"
+  sku_name            = "B1"
 }
 
 # Create App Service
@@ -21,7 +21,6 @@ resource "azurerm_windows_web_app" "app_service" {
   service_plan_id     = azurerm_service_plan.app_plan.id
 
   site_config {
-    dotnet_version = "v8.0"
   }
 
   app_settings = {
@@ -36,4 +35,22 @@ resource "azurerm_storage_account" "storage_account" {
   location                 = azurerm_resource_group.rg.location
   account_tier              = "Standard"
   account_replication_type = var.storage_account_sku
+}
+
+# Create SQL Server
+resource "azurerm_mssql_server" "sql_server" {
+  name                         = var.sql_server_name
+  resource_group_name           = azurerm_resource_group.rg.name
+  location                     = azurerm_resource_group.rg.location
+  version                      = "12.0"
+  administrator_login          = var.sql_admin_username
+  administrator_login_password = var.sql_admin_password
+}
+
+# Create SQL Database
+resource "azurerm_mssql_database" "sql_database" {
+  name                = var.sql_database_name
+  server_id           = azurerm_mssql_server.sql_server.id
+  sku_name            = "S1"
+  collation           = "SQL_Latin1_General_CP1_CI_AS"
 }
