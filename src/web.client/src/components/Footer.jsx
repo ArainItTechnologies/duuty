04f95636e78cb69bedb738a5d799872a3c8fdf8d
Lenin <leninmariajoseph@gmail.com>
2025-05-25 11:25:52 +0400

    mobile menu changes

--- src/web.client/src/components/Footer.jsx
+++ src/web.client/src/components/Footer.jsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
+import LogoSrc from '../assets/logo.svg'
 
 const Footer = () => {
   return (
-    <footer className="bg-zinc-50 text-center text-surface/75 dark:bg-neutral-700 dark:text-white/75 lg:text-left">
-      <div className="flex items-center justify-center border-b-2 border-neutral-200 p-6 dark:border-white/10 lg:justify-between">
+    <footer className="container-wrapper text-center text-surface/75 lg:text-left">
+      {/* <div className="flex items-center justify-center border-b-2 border-neutral-200 p-6 dark:border-white/10 lg:justify-between">
         <div className="me-12 hidden lg:block">
           <span>Get connected with us on social networks:</span>
         </div>
@@ -63,57 +64,110 @@ const Footer = () => {
             </svg>
           </a>
         </div>
-      </div>
+      </div> */}
 
-      <div className="mx-6 py-6 text-center md:text-left">
-        <div className="grid-1 grid gap-8 md:grid-cols-2 lg:grid-cols-4">
+      <div className="py-6 text-center md:text-left">
+        <div className="grid-1 grid gap-8 lg:gap-12 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3">
           <div className="">
             <h6 className="mb-4 flex items-center justify-center font-semibold uppercase md:justify-start">
-              <span className="me-3 [&>svg]:h-4 [&>svg]:w-4">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                >
-                  <path d="M12.378 1.602a.75.75 0 00-.756 0L3 6.632l9 5.25 9-5.25-8.622-5.03zM21.75 7.93l-9 5.25v9l8.628-5.032a.75.75 0 00.372-.648V7.93zM11.25 22.18v-9l-9-5.25v8.57a.75.75 0 00.372.648l8.628 5.033z" />
-                </svg>
+              <span className="">
+              <img
+                    alt="Duuty Logo"
+                    src={LogoSrc}
+                    className="w-[130px]"
+                  />
               </span>
-              TW Elements
             </h6>
             <p>
-              Here you can use rows and columns to organize your footer content.
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Connecting great kitchens with great talent
             </p>
+            <div className="flex justify-center sm:justify-start mt-4 space-x-4 mb-4">
+              <a href="#!" className="[&>svg]:h-5 [&>svg]:w-5">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 320 512"
+                >
+                  <path d="M80 299.3V512H196V299.3h86.5l18-97.8H196V166.9c0-51.7 20.3-71.5 72.7-71.5c16.3 0 29.4 .4 37 1.2V7.9C291.4 4 256.4 0 236.2 0C129.3 0 80 50.5 80 159.4v42.1H14v97.8H80z" />
+                </svg>
+              </a>
+              <a href="#!" className="[&>svg]:h-5 [&>svg]:w-5">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 512 512"
+                >
+                  <path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z" />
+                </svg>
+              </a>
+              <a href="#!" className="[&>svg]:h-5 [&>svg]:w-5">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 488 512"
+                >
+                  <path d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z" />
+                </svg>
+              </a>
+            </div>
           </div>
           <div>
-            <h6 className="mb-4 flex justify-center font-semibold uppercase md:justify-start">
-              Products
+            <h6 className="mb-4 flex justify-center font-bold md:justify-start">
+            Company
             </h6>
             <p className="mb-4">
-              <a href="#!">Angular</a>
+              <a href="#!">About Us</a>
             </p>
             <p className="mb-4">
-              <a href="#!">React</a>
+              <a href="#!">Privacy Policy</a>
             </p>
-            <p className="mb-4">
-              <a href="#!">Vue</a>
-            </p>
+            {/* <p className="mb-4">
+              <a href="#!">Mobile</a>
+            </p> */}
           </div>
           <div>
-            <h6 className="mb-4 flex justify-center font-semibold uppercase md:justify-start">
-              Useful links
+            <h6 className="mb-4 flex justify-center font-bold md:justify-start">
+            Contact
             </h6>
             <p className="mb-4">
-              <a href="#!">Pricing</a>
+              <a href="/#faq-section">Help/FAQ</a>
             </p>
             <p className="mb-4">
-              <a href="#!">Settings</a>
+              <a href="#!">Contact Us</a>
+            </p>
+            {/* <p className="mb-4">
+              <a href="#!">Affilates</a>
+            </p> */}
+          </div>
+          {/* <div>
+            <h6 className="mb-4 flex justify-center font-bold md:justify-start">
+              More
+            </h6>
+            <p className="mb-4">
+              <a href="#!">Airlinefees</a>
             </p>
             <p className="mb-4">
-              <a href="#!">Orders</a>
+              <a href="#!">Airline</a>
+            </p>
+            <p className="mb-4">
+              <a href="#!">Low fare tips</a>
             </p>
           </div>
           <div>
+            <h6 className="mb-4 flex justify-center font-bold md:justify-start">
+              More
+            </h6>
+            <p className="mb-4">
+              <a href="#!">Airlinefees</a>
+            </p>
+            <p className="mb-4">
+              <a href="#!">Airline</a>
+            </p>
+            <p className="mb-4">
+              <a href="#!">Low fare tips</a>
+            </p>
+          </div> */}
+          {/* <div>
             <h6 className="mb-4 flex justify-center font-semibold uppercase md:justify-start">
               Contact
             </h6>
@@ -159,16 +213,16 @@ const Footer = () => {
               </span>
               +91 8939785306
             </p>
-          </div>
+          </div> */}
         </div>
       </div>
 
-      <div className="bg-black/5 p-6 text-center">
+      {/* <div className="bg-black/5 p-6 text-center">
         <span>© 2023 Copyright:</span>
         <Link className="font-semibold" to="https://duuty.in/">
           Duuty
         </Link>
-      </div>
+      </div> */}
     </footer>
   );
 };
